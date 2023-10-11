5aac620451d80a209cd23d60028dd75f992af5d4
echeadle <echeadle@gmail.com>
2023-10-10 22:09:55 -0600

    use chatgpt, not langchain and abstract the query.

--- code-togo/intro-chat-promts.py
+++ code-togo/intro-chat-promts.py
@@ -6,13 +6,11 @@ from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
 
 load_dotenv(find_dotenv())
-openai.api_key = os.getenv("OPEN_API_KEY")
-
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 llm_model = "gpt-3.5-turbo"
 
-chat_model = ChatOpenAI(temperature=0.7, model=llm_model)
-
+# OpenAI Completion Endpoint
 def get_completion(prompt, model=llm_model):
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
@@ -21,3 +19,24 @@ def get_completion(prompt, model=llm_model):
         temperature=0,
     )
     return response.choices[0].message["content"]
+
+# Translate text, review
+customer_review = """
+ Your product is terrible!  I don't know how 
+ you were able to get this to the market.
+ I don't want this! Actually no one should want this.
+ Seriously!  Give me money now!
+ 
+"""
+tone = """ Proper British English in a nice, warm, respectful tone """
+language = "Turkish"
+promp = f"""
+    Rewrite the following {customer_review}  in {tone}, and then
+    please translate the new review message into {language}.
+"""
+
+if __name__ == "__main__":
+
+    rewrite = get_completion(prompt=promp)
+    print(rewrite)
+    
